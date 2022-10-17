05e83dc1b7baccc855eec422103d9fe588b2b4f0
Novikov Sergey <sergey.novikov@flaconi.de>
2022-10-17 14:05:22 +0200

    Update SQS module (#73)

--- sqs4.tf
+++ sqs4.tf
@@ -4,7 +4,7 @@ locals {
 
 module "sqs4" {
   source  = "terraform-aws-modules/sqs/aws"
-  version = "~> 2.0"
+  version = "3.4.1"
 
   create = var.sqs4_enabled ? "true" : "false"
   name   = local.sqs4_name
@@ -13,13 +13,13 @@ module "sqs4" {
   fifo_queue                 = var.sqs4_fifo_queue
   max_message_size           = var.sqs4_max_message_size
   receive_wait_time_seconds  = var.sqs4_receive_wait_time_seconds
-  redrive_policy             = var.sqs4_dlq_enabled ? "{\"deadLetterTargetArn\":\"${module.sqs4-dlq.this_sqs_queue_arn}\",\"maxReceiveCount\":4}" : var.sqs4_redrive_policy
+  redrive_policy             = var.sqs4_dlq_enabled ? "{\"deadLetterTargetArn\":\"${module.sqs4-dlq.sqs_queue_arn}\",\"maxReceiveCount\":4}" : var.sqs4_redrive_policy
   visibility_timeout_seconds = var.sqs4_visibility_timeout_seconds
 }
 
 module "sqs4-dlq" {
   source  = "terraform-aws-modules/sqs/aws"
-  version = "~> 2.0"
+  version = "3.4.1"
 
   create = var.sqs4_dlq_enabled ? "true" : "false"
   name   = "${local.sqs4_name}-deadletter"
